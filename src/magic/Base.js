df9f8de0c82db368d90147038ac4d3b1175f0b8d
baiduFE <tangram@baidu.com>
2012-02-13 17:12:06 +0800

    merge提交上来的base代码 [Feature] 处理有冲突的代码 Base.js : 一些不合文档注释的修改

--- src/magic/Base.js
+++ src/magic/Base.js
@@ -3,8 +3,8 @@
  * Copyright 2009 Baidu Inc. All rights reserved.
  * 
  * path: baidu/lang/Class.js
- * author: meizz
- * version: 0.1
+ * author: meizz, erik
+ * version: 2.0
  * date: 2011/11/24
  */
 
@@ -17,19 +17,18 @@
 /**
  * UI基类，所有的UI都应该从这个类中派生出去
  * @name magic.Base
- * @grammar new magic.Base()
+ * @grammar magic.Base
  * @class
+ * @return {magic.Base}
  * @author meizz
  */
 magic.Base = function(){
     baidu.lang.Class.call(this);
 
     this._ids = {};
-    this._eid = this.__type.replace(/\W/g, "_") +"__"+ this.guid +"__";
+    this._eid = this.guid +"__";
 }
-baidu.lang.inherits(magic.Base, baidu.lang.Class, "magic.Base").extend(
-/** @lends magic.Base.prototype */
-{
+baidu.lang.inherits(magic.Base, baidu.lang.Class, "magic.Base").extend({
     /**
      * 取得 ui 模块对应的 dom element 对象
      * @param   {String}    key     该ID对应的关键字(可选参数)
@@ -39,7 +38,7 @@ baidu.lang.inherits(magic.Base, baidu.lang.Class, "magic.Base").extend(
         return document.getElementById(this.getId(id));
     }
 
-    /**
+    /*
      * 取得一个页面唯一的 id
      * @param   {String}    key     该ID对应的关键字(可选参数)
      * @return  {String}            页面唯一的 id，可以作为DOM元素的id
@@ -68,10 +67,11 @@ baidu.lang.inherits(magic.Base, baidu.lang.Class, "magic.Base").extend(
      * 析构函数，在析构时派发析构事件
      */
     ,dispose : function() {
-        this.dispatchEvent("ondispose") && baidu.lang.Class.prototype.dispose.call(this);
+        this.fire("ondispose") && baidu.lang.Class.prototype.dispose.call(this);
     }
 });
 
+//  20120110    meizz   简化eid去掉其中的__type部分；事件派发使用fire方法替换原来 dispatchEvent
 //  20111129    meizz   实例化效率大比拼
 //                      new ui.Base()           效率为 1
 //                      new ui.control.Layer()  效率为 2
