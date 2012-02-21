89515d838ab913792d90dad2083e2671e3ddcc30
jianling <zhaochengyang@baidu.com>
2012-02-21 14:53:24 +0800

    修改demo

--- demos/magic.Suggestion/service.php
+++ demos/magic.Suggestion/service.php
@@ -1,5 +1,5 @@
-<?php
-    header("Content-Type:application/javascript");
+﻿<?php
+    @header("Content-Type:application/javascript");
     $key = $_GET["key"];
     if($key == 'f'){
         // sleep(1);
@@ -7,5 +7,5 @@
     if($key != 'd')
         echo "[{value:'$key+1value',content:'$key+1'},{value:'北海2value',content:'北海2'},{value:'北海3value',content:'北海3'},{value:'北海4value',content:'北海4',disable:true},{value:'北海5value',content:'北海5'}]";
     else 
-        echo '[]';
+        echo '["<input>\'\"","<>","<input> \'\"test it","<input/>"]';
 ?>
