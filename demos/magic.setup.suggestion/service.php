cb959dbc24bc3869e56af57a1dd6ffeff8ca98f2
linlingyu <linlingyu@baidu.com>
2012-03-07 15:09:24 +0800

    修改六大组件的css和demo结构 [Doc]当前的目录结构不清晰，在codesearch上无法拿到setup方式的资源 demo目录结构，resources目录结构

--- demos/magic.setup.suggestion/service.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header("Content-Type:application/javascript");
-    $key = $_GET["key"];
-    if($key == 'f'){
-        // sleep(1);
-    }
-    echo "[{value:'$key+1value',content:'$key+1'},{value:'北海2value',content:'北海2'},{value:'北海3value',content:'北海3'},{value:'北海4value',content:'北海4',disable:true},{value:'北海5value',content:'北海5'}]";
-?>
