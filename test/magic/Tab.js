bd57f09d4a2b11e709ab5b85314c09efc20bea1d
tianlili <tianlili@b000000020050.internal.baidu.com>
2012-03-27 16:20:56 +0800

    根据Magic统一升级更新用例
    [Testcase]根据Magic统一升级更新用例
    tab：根据Magic统一升级更新用例

--- test/magic/Tab.js
+++ test/magic/Tab.js
@@ -15,9 +15,9 @@ test("render, default param", function(){
 	        ]
 	    });
 		tab.render('div1');
-		equals(tab._options.toggleEvent, 'click', "The toggleEvent is right");
-		equals(tab._options.toggleDelay, 0, "The toggleDelay is right");
-		equals(tab._options.selectedIndex, 0, "The selectedIndex is right");
+		equals(tab._options.selectEvent, 'click', "The selectEvent is right");
+		equals(tab._options.selectDelay, 0, "The selectDelay is right");
+		equals(tab._options.originalIndex, 0, "The originalIndex is right");
 		equals(tab._selectedIndex, 0, "The _selectedIndex is right");
 		equals($("li a span", tab.getElement("title")).text(), "项目一项目二项目三", "The title is right");
 		equals($("div", tab.getElement("body")).text(), "hello world~1hello world~2hello world~3", "The title is right");
@@ -34,9 +34,9 @@ test("render, all param", function(){
 	document.body.appendChild(div);
 	div.id = "div1";
 	var tab = new magic.Tab({
-		'selectedIndex' : 2,
-        'toggleEvent' : 'mouseover',
-        'toggleDelay' : 1000,
+		'originalIndex' : 2,
+        'selectEvent' : 'mouseover',
+        'selectDelay' : 1000,
         'items': [
             {title: '项目一', content: 'hello world~1'},
             {title: '项目二', content: 'hello world~2'},
@@ -44,9 +44,9 @@ test("render, all param", function(){
         ]
     });
 	tab.render('div1');
-	equals(tab._options.toggleEvent, 'mouseover', "The toggleEvent is right");
-	equals(tab._options.toggleDelay, 1000, "The toggleDelay is right");
-	equals(tab._options.selectedIndex, 2, "The selectedIndex is right");
+	equals(tab._options.selectEvent, 'mouseover', "The selectEvent is right");
+	equals(tab._options.selectDelay, 1000, "The selectDelay is right");
+	equals(tab._options.originalIndex, 2, "The originalIndex is right");
 	equals(tab._selectedIndex, 2, "The _selectedIndex is right");
 	equals($("li a span", tab.getElement("title")).text(), "项目一项目二项目三", "The title is right");
 	equals($("div", tab.getElement("body")).text(), "hello world~1hello world~2hello world~3", "The title is right");
@@ -55,14 +55,16 @@ test("render, all param", function(){
 	document.body.removeChild(div);
 });
 
-test("focus", function(){
-	expect(9);
+test("select", function(){
+	expect(15);
 	var div = document.createElement("div");
 	document.body.appendChild(div);
 	div.id = "div1";
+	var beforeselect = 0;
+	var select = 0;
 	var l1 = baidu.event._listeners.length;
 	var tab = new magic.Tab({
-		'selectedIndex' : 2,
+		'originalIndex' : 2,
         'items': [
             {title: '项目一', content: 'hello world~1'},
             {title: '项目二', content: 'hello world~2'},
@@ -70,13 +72,31 @@ test("focus", function(){
         ]
     });
 	tab.render('div1');
-	tab.focus(0);
+	tab.on("onbeforeselect", function(e, data){
+		beforeselect ++;
+		if(beforeselect == 1)
+			equals(data.index, 2, "The onbeforeselect is right");
+		if(beforeselect == 2)
+			equals(data.index, 0, "The onbeforeselect is right");
+		if(beforeselect == 3)
+			equals(data.index, 2, "The onbeforeselect is right");
+	})
+	tab.on("onselect", function(e, data){
+		select ++;
+		if(select == 1)
+			equals(data.index, 0, "The onselect is right");
+		if(select == 2)
+			equals(data.index, 2, "The onselect is right");
+		if(select == 3)
+			equals(data.index, 2, "The onselect is right");
+	})
+	tab.select(0);
 	equals(tab._selectedIndex, 0, "The _selectedIndex is right");
 	equals($(".tang-body-item-selected", tab.getElement("body")).text(), "hello world~1", "The current tab is right");
-	tab.focus(2);
+	tab.select(2);
 	equals(tab._selectedIndex, 2, "The _selectedIndex is right");
 	equals($(".tang-body-item-selected", tab.getElement("body")).text(), "hello world~3", "The current tab is right");
-	tab.focus(2);
+	tab.select(2);
 	equals(tab._selectedIndex, 2, "The _selectedIndex is right");
 	equals($(".tang-body-item-selected", tab.getElement("body")).text(), "hello world~3", "The current tab is right");
 	tab.dispose();
@@ -93,8 +113,8 @@ test("click", function(){
 	document.body.appendChild(div);
 	div.id = "div1";
 	var tab = new magic.Tab({
-		'selectedIndex' : 2,
-        'toggleEvent' : 'click',
+		'originalIndex' : 2,
+        'selectEvent' : 'click',
         'items': [
             {title: '项目一', content: 'hello world~1'},
             {title: '项目二', content: 'hello world~2'},
@@ -116,8 +136,8 @@ test("mouseover", function(){
 	document.body.appendChild(div);
 	div.id = "div1";
 	var tab = new magic.Tab({
-		'selectedIndex' : 2,
-        'toggleEvent' : 'mouseover',
+		'originalIndex' : 2,
+        'selectEvent' : 'mouseover',
         'items': [
             {title: '项目一', content: 'hello world~1'},
             {title: '项目二', content: 'hello world~2'},
