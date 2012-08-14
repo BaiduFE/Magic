70a7821725349289c3494dab6ac84b0e2f7143f8
wangxiao <1988wangxiao@gmail.com>
2012-08-14 15:39:25 +0800

    修改config.php

--- test/tools/br/config.php
+++ test/tools/br/config.php
@@ -34,7 +34,7 @@ class Config{
 	 * @var ArrayIterator::String
 	 */
 	public static $SOURCE_PATH = array("../../../src/",
-		"../../../../Tangram2/src/",
+		"../../../../Tangram-base/src/",
 	    "../../../../Tangram-component/src/",
 		"../../../../tangram/src/"
 		);
