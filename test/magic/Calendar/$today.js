61cb770c61142344ad53ddbebdf07a7078259f87
Robinlim <vicxin1010@hotmail.com>
2012-12-14 16:43:19 +0800

    add css file to test case

--- test/magic/Calendar/$today.js
+++ test/magic/Calendar/$today.js
@@ -5,48 +5,50 @@ test("test plugin enable", function(){
 	expect(4);
     stop();
     ua.importsrc('magic.Calendar.$timer,baidu.dom.text,baidu.i18n.cultures.en-US' ,function(){
-	    var container = document.createElement("div");
-	    document.body.appendChild(container);
-	    //插件不启用
-	    var ca = new magic.Calendar({
-	        initDate: new Date("2012/05/06"),
-	        today: {
-	        	enable: false
-	        }
-	    });
-	    ca.render(container);
-	    
-	    equals(ca.getElement("today"), undefined, "不启用插件，回到今天区域不存在");
-	    ca.$dispose();
+    	ua.loadcss(upath + "./magic.Calendar.css", function(){
+		    var container = document.createElement("div");
+		    document.body.appendChild(container);
+		    //插件不启用
+		    var ca = new magic.Calendar({
+		        initDate: new Date("2012/05/06"),
+		        today: {
+		        	enable: false
+		        }
+		    });
+		    ca.render(container);
+		    
+		    equals(ca.getElement("today"), undefined, "不启用插件，回到今天区域不存在");
+		    ca.$dispose();
 
-	    //插件启用 
-	    //中文
-	    ca = new magic.Calendar({
-	        initDate: new Date("2012/05/06"),
-	        today:{
-	            enable: true
-	        }
-	    });
-	    ca.render(container);
-	    ok(ca.getElement("today") != undefined, "启用插件，回到今天区域在底部区域");
-	    equals(baidu(ca.getElement("today")).text(), '今天', '回到今天区域label中文为：今天');
-	    ca.$dispose();
+		    //插件启用 
+		    //中文
+		    ca = new magic.Calendar({
+		        initDate: new Date("2012/05/06"),
+		        today:{
+		            enable: true
+		        }
+		    });
+		    ca.render(container);
+		    ok(ca.getElement("today") != undefined, "启用插件，回到今天区域在底部区域");
+		    equals(baidu(ca.getElement("today")).text(), '今天', '回到今天区域label中文为：今天');
+		    ca.$dispose();
 
-	    //英文
-		ca = new magic.Calendar({
-	        initDate: new Date("2012/05/06"),
-	        today:{
-	            enable: true
-	        },
-	        language: 'en-US'
-	    });
-	    ca.render(container);
-	    equals(baidu(ca.getElement("today")).text(), 'Today', '回到今天区域label英文为：Today');
+		    //英文
+			ca = new magic.Calendar({
+		        initDate: new Date("2012/05/06"),
+		        today:{
+		            enable: true
+		        },
+		        language: 'en-US'
+		    });
+		    ca.render(container);
+		    equals(baidu(ca.getElement("today")).text(), 'Today', '回到今天区域label英文为：Today');
 
 
-	    start();
-	    ca.$dispose();
-	    document.body.removeChild(container);
+		    start();
+		    ca.$dispose();
+		    document.body.removeChild(container);
+		});
 	}, 'baidu.i18n.cultures.en-US', 'magic.Calendar.$today');
 });
 
