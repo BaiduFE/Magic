efa950255e26fcf2f92fffad1ca8a7d4a0c459a0
techird <techirdliu@gmail.com>
2013-01-04 11:42:31 +0800

    [Demo] Demo 控制台于底部停靠，并且添加收缩功能
    1. 收缩功能用cookie记录历史状态
    2. 对 Demo.js 进行了简单重构，使其更易读

--- demos/common/demo.js
+++ demos/common/demo.js
@@ -1,31 +1,72 @@
-///import baidu.dom.height;
-
-function log(type, msg){
+function log(type, msg) {
     baidu('#J_console').append('<p><span class="' + type + '">' + type + '</span>' + msg + '</p>');
     baidu('#J_console').get(0).scrollTop = 999999;
 }
+
 baidu(function (){
-    var list = baidu('div.demo-list a');
-    var nameMatcher = /\/(.+\.html)/;
-    var nameMatch = nameMatcher.exec(window.location.href);
-    var currentName = nameMatch && nameMatch[1];
-    var i; 
-    for(i=0; i < list.length; ++i) {
-        var a = list[i];
-        var nameMatch = nameMatcher.exec(a.href);
-        var name = nameMatch && nameMatch[1];
-        if( name == currentName ) {
-            baidu(a).addClass("selected");
+    (function makeNavigationInBlank() {
+        baidu('div.demo-list a').each( function( index ) {
+            baidu(this).after('<a class="open-blank" href="' + this.href + '" target="_blank" title="在新窗口打开"></a>');
+        });
+    })();
+
+    (function highlightCurrentNav() {
+        var nameMatcher = /\/(.+\.html)/;
+        function matchName( href ) {
+            var nameMatch = nameMatcher.exec(href);
+            return nameMatch && nameMatch[1];
         }
-        baidu(a).after('<a class="open-blank" href="' + a.href + '" target="_blank" title="在新窗口打开"></a>')
-    };
-    baidu.browser.ie < 8 && baidu('body').height() < 500 && baidu('body').height( 500 );
-    window.top != window && baidu('body').addClass('iframe');
-    baidu('<a>')
-        .text('清空')
-        .attr('title','清空日志内容')
-        .addClass('clear-console')
-        .click( function() {
-            baidu('#J_console').empty();
-        }).appendTo('.console-wrap');
+        var currentName = matchName( window.location.href );
+        baidu('div.demo-list a').filter( function( index ) {
+            return matchName( this.href ) === currentName;
+        }).addClass('selected');
+    })();
+
+    (function autoHeightForIE() {        
+        baidu.browser.ie < 8 && baidu('body').height() < 500 && baidu('body').height( 500 );
+    })();
+
+    (function recognizeIFrame() {        
+        window.top != window && baidu('body').addClass('iframe');
+    })();
+
+    (function addClearButtonForLogger() {        
+        baidu('<a>')
+            .text('清空')
+            .attr('title','清空日志内容')
+            .addClass('clear-console')
+            .click( function() {
+                baidu('#J_console').empty();
+            }).appendTo('.console-wrap');
+    })();
+
+    (function makeLoggerExpandable(){        
+        var expander = baidu('<a>').addClass('console-expander').appendTo('.console-wrap');
+        var cookieName = 'demo-default-expanded';  
+        function expand(){
+            baidu('#J_console').css('height', 200);
+            baidu('.main-wrap').css('margin-bottom', 235);
+            expander.addClass('expanded');
+            baidu.cookie.set( cookieName, true, { expires: Number.MAX_VALUE } );
+        }
+        function collapse(){
+            baidu('#J_console').css('height', 0);                
+            baidu('.main-wrap').css('margin-bottom', 35);
+            expander.removeClass('expanded');
+            baidu.cookie.remove( cookieName );
+        }
+        function toggle() {          
+            expander.hasClass('expanded') ? collapse() : expand();
+        }
+        expander.click(toggle);
+        baidu.cookie.get(cookieName) ? expand() : collapse();
+
+        // 绑定快捷键和快捷键说明
+        baidu('.console-wrap h3').text('控制台 ( Ctrl + F12 )');
+        baidu(window).keydown(function(e){
+            if( e.ctrlKey && e.keyCode == 123 ) {
+                expander.click();
+            }
+        });
+    })();  
 });
